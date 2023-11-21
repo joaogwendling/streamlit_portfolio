66f144f82aa2f88771b5fdbba1b11178cbc4302c
joaogwendling <54818262+joaogwendling@users.noreply.github.com>
2023-11-21 15:21:33 -0300

    Changed path var

--- Estudos2_Portfolio/joaogwendling.py
+++ Estudos2_Portfolio/joaogwendling.py
@@ -5,7 +5,7 @@ from PIL import Image
 import os
 import openpyxl
 
-path = '/mount/src/streamlit_portfolio/Estudos2_Portfolio'
+path = '/app/streamlit_portfolio/Estudos2_Portfolio'
 
 st.set_page_config(
         page_title="João Wendling - Portfolio",
